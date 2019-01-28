1558a79aa0ad770f6abdc16dccb5b0e83561151c
Jody Phelan <jody.phelan@lshmt.ac.uk>
2019-01-28 14:24:22 +0000

    init

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,6 @@
 python split_fa.py HIV_pol.consensus.renamed.fas
 cat index.txt | parallel --col-sep "\t" "wgsim {1}.fa {2}_1.fq {2}_2.fq -N 500 -e 0.005 -1 150 -2 150"
+bwa index ref.fa 
 bwa mem ref.fa HIV.pol.sample09_1.fq | samtools view -b - | samtools sort -o tmp.bam -
 samtools index tmp.bam
 python  simulate_mixed_call.py 0.1 | python remove_sames.py | gzip -c > tmp.fq.gz
@@ -8,10 +9,10 @@ cut -f2 index.txt > samples.txt
 cat samples.txt | parallel "cat {}_1.fq | python remove_sames.py | gzip -c > {}.fq.gz"
 mv tmp.fq.gz HIV.pol.sample09.fq.gz
 
-rm *_1.fq rm *_2.fq tmp*
+rm *_1.fq *_2.fq tmp*
+cut -f1 index.txt | parallel "rm {}.fa"
 
-#bwa index ref.fa 
-#cat samples.txt | parallel "bwa mem ref.fa {}.fq.gz | samtools view -b - | samtools sort -o {}.bam -"
+##cat samples.txt | parallel "bwa mem ref.fa {}.fq.gz | samtools view -b - | samtools sort -o {}.bam -"
 #cat samples.txt | parallel "samtools index {}.bam"
 #cat samples.txt |  parallel "bcftools mpileup -f ref.fa {}.bam | bcftools call -mvOz > {}.vcf.gz"
 #cat samples.txt | parallel "bcftools index {}.vcf.gz"
